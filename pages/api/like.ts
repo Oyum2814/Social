18a0ce597d91f2256e2f990f7f2e89d5c9a2ddcf
Om <ommukherjee28@gmail.com>
2024-01-01 03:28:40 +0530

    fix:faster like

--- pages/api/like.ts
+++ pages/api/like.ts
@@ -14,11 +14,14 @@ export default async function handler(req:NextApiRequest, res:NextApiResponse)
             throw new Error('Invalid ID');
         }
 
-        const post = await prismadb.post.findUnique({
-            where:{
-                id:postId
+        let post: any;
+
+        post = await prismadb.post.findUnique({
+            where: {
+                id: postId
             }
         });
+
         if(!post){
             throw new Error('Invalid ID');
         }
@@ -28,11 +31,6 @@ export default async function handler(req:NextApiRequest, res:NextApiResponse)
         if(req.method==='POST'){
             updatedLikedIds.push(currentUser?.id); 
             try{
-                const post = await prismadb.post.findUnique({
-                    where: {
-                        id: postId
-                    }
-                });
                 if(post?.userId && post?.userId!==currentUser?.id){
                     await prismadb.notification.create({
                         data:{
@@ -56,9 +54,9 @@ export default async function handler(req:NextApiRequest, res:NextApiResponse)
             }
         }
         if(req.method==='DELETE'){
-            updatedLikedIds = updatedLikedIds.filter((likedId)=>{
+            updatedLikedIds =await Promise.all(updatedLikedIds.filter((likedId)=>{
                 likedId !== currentUser?.id
-            });
+            }));
         }
         const updatedPost =await prismadb.post.update({
             where:{
